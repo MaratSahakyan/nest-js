ec44e4a6067221bf9597ca7b8af3466f1e7ddb66
FIRST_NAME LAST_NAME <sahakyanmarat747@mail.ru>
2023-05-17 00:36:11 +0400

    in progress nest js

--- src/users/controller/users/users.controller.ts
+++ src/users/controller/users/users.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { Request, response, Response } from 'express';
 import { CreateUserDto } from '../../dtos/CreateUser.dto';
 @Controller('users')
 export class UsersController {
@@ -11,5 +12,11 @@ export class UsersController {
   createUser(@Body() userData: CreateUserDto) {
     console.log('userData', userData);
     return {};
+  }
+
+  @Get(':id')
+  getUserById(@Req() request: Request, @Res() resonse: Response) {
+    console.log(request.params);
+    response.send('');
   }
 }
